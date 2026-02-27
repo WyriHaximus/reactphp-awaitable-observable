9c6c0b70c5099b5b38997d6d810f242e238751a9
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2026-02-27 19:30:50 +0100

    Awaiting observables should always have been lazy

--- etc/qa/rector.php
+++ etc/qa/rector.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use WyriHaximus\TestUtilities\RectorConfig;
 
-return RectorConfig::configure(dirname(__DIR__, 2));
+return RectorConfig::configure(dirname(__DIR__, 2))->withSkip([
+    ClassPropertyAssignToConstructorPromotionRector::class,
+]);
